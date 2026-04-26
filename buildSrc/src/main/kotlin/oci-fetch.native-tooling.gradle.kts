499604e9f8eac95a2dffc4b477bfbfb2d055b28d
Dimitar Dimitrov <dimitar.dimitrov@gmail.com>
2026-04-26 16:29:02 +0900

    Fix `ktor-client-curl` linking issues for cross-compilation on LinuxX64

--- buildSrc/src/main/kotlin/oci-fetch.native-tooling.gradle.kts
+++ buildSrc/src/main/kotlin/oci-fetch.native-tooling.gradle.kts
@@ -10,3 +10,10 @@ tasks.register<Exec>("linuxX64TestInWSL") {
 
     commandLine("wsl", "bash", "-c", "\"$(wslpath $testExecutable)\"")
 }
+
+tasks.matching { it.name.lowercase().matches(Regex("^linuxx64.*|.*linuxx64$")) }.configureEach {
+    onlyIf("KTOR-3965 ktor-client-curl has linking issues for cross-compilation") {
+        !System.getProperty("os.name").lowercase().contains("windows")
+    }
+}
+
